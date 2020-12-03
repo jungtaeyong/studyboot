2c2a0f3ffac1cb441636cbb21bd3d5be572de4be
jungtaeyong <ckck5050@naver.com>
2020-12-03 16:04:12 +0900

    JPA 적용 및 수정/조회/삽입 Dto 구현

---
+++ src/main/java/com/taeyong/book/studyboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.taeyong.book.studyboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
---
+++ src/main/java/com/taeyong/book/studyboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,22 @@
+package com.taeyong.book.studyboot.web.dto;
+
+
+import com.taeyong.book.studyboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content =entity.getContent();
+        this.author=entity.getAuthor();
+    }
+
+}
